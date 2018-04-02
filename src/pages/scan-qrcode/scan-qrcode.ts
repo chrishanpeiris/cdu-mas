8566a714b36ac56667b08f083496d07b0a78f0dc
chrishan <chrishanmx@gmail.com>
2018-04-02 10:08:46 +0930

    UI prototype
    Added UI prototype.

---
+++ src/pages/scan-qrcode/scan-qrcode.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-scan-qrcode',
+  templateUrl: 'scan-qrcode.html'
+})
+export class ScanQRCodePage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
