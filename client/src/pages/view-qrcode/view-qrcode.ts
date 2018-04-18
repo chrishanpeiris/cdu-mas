d77c1db45f18a8ebb1437aa995eeb61c6c3ab694
Pasindu Danthaka Sri <pasinduworks92@gmail.com>
2018-04-19 03:06:41 +0930

    QR code creation completed
    QR code generation completed

--- client/src/pages/view-qrcode/view-qrcode.ts
+++ client/src/pages/view-qrcode/view-qrcode.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { StudentHomePage } from '../student-home/student-home';
+import {BarcodeScanner} from "@ionic-native/barcode-scanner";
+import {AuthProvider} from "../../providers/auth/auth";
 //import { ViewQRCodePage } from '../view-qrcode/view-qrcode';
 
 @Component({
@@ -9,8 +11,32 @@ import { StudentHomePage } from '../student-home/student-home';
 })
 export class ViewQRCodePage {
 
-  constructor(public navCtrl: NavController) {
+  createdCode:any;
+  user:any;
+
+  constructor(public navCtrl: NavController,private barcodeScanner: BarcodeScanner, public authProvider: AuthProvider) {
+
+    this.createCode();
+
   }
+
+
+  createCode(){
+    this.getUserCode();
+    // this.createdCode=this.student_code;
+    console.log("QRcode Generated");
+  }
+
+  getUserCode(){
+    this.authProvider.getUser()
+      .then(data =>{
+        this.user=data;
+        this.createdCode=this.user.student_id;
+        console.log(this.createdCode);
+
+      });
+  }
+
   goToStudentHome(params){
     if (!params) params = {};
     this.navCtrl.push(StudentHomePage);
