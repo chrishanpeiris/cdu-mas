3f57ab8d275b414d0bb920edea58d9083dbadaba
chrishanpeiris <chrishanmx@gmail.com>
2018-04-26 00:23:00 +0930

    Merged branch chrishan to dev.

---
+++ client/src/pages/login/login.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import {AlertController, NavController} from 'ionic-angular';
+import {TabsStudentPage} from '../tabs-student/tabs-student';
+import {TabsLecturePage} from '../tabs-lecture/tabs-lecture'
+import { ViewQRCodePage } from '../view-qrcode/view-qrcode';
+import { SignupPage } from '../signup/signup';
+import { ForgotPasswordPage } from '../forgot-password/forgot-password';
+import {AuthProvider} from "../../providers/auth/auth";
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  user = {email: '', password: ''};
+  authresponse: any;
+  autherrors: any;
+  errormessage: any;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public AuthProvider: AuthProvider) {
+  }
+  goToStudentHome(params){
+    this.AuthProvider.userLogin(this.user).then((result) => {
+      this.authresponse = result;
+      console.log(this.authresponse.access_token);
+      if (this.authresponse != null) {
+        this.AuthProvider.storeToken(this.authresponse.access_token);
+        this.navCtrl.push(TabsLecturePage);
+      }
+    }, (err) => {
+      this.autherrors = err;
+
+      if (err.statusText = "Unauthorized") {
+        this.showLoginAlert();
+        console.log(this.errormessage);
+      }
+      console.log(this.autherrors);
+    });
+  }
+  goToViewQRCode(params){
+    if (!params) params = {};
+    this.navCtrl.push(ViewQRCodePage);
+  }
+  goToSignup(params){
+    if (!params) params = {};
+    this.navCtrl.push(SignupPage);
+  }
+  goToForgotPassword(params){
+    if (!params) params = {};
+    this.navCtrl.push(ForgotPasswordPage);
+  }
+
+  showLoginAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Oops! Login failed!',
+      subTitle: 'Incorrect username or password, Please check and try again',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+}
