c3e044f02068727e28855f968c6d9a6913789099
Pasindu Danthaka Sri <pasinduworks92@gmail.com>
2018-05-03 12:53:38 +0930

    kjkj
    kkjk

--- backend/app/Http/Middleware/TrustProxies.php
+++ backend/app/Http/Middleware/TrustProxies.php
@@ -14,16 +14,26 @@ class TrustProxies extends Middleware
      */
     protected $proxies;
 
+
+
     /**
      * The current proxy header mappings.
      *
      * @var array
      */
-    protected $headers = [
+    /*protected $headers = [
         Request::HEADER_FORWARDED => 'FORWARDED',
         Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
         Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
         Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
         Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
-    ];
+    ];*/
+
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var string
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
