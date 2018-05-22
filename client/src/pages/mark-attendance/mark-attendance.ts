8f561fba845a90d98e727f4a7a565da03c4b4628
Pasindu Danthaka Sri <pasinduworks92@gmail.com>
2018-05-22 23:06:45 +0930

    mark students
    mark students

--- client/src/pages/mark-attendance/mark-attendance.ts
+++ client/src/pages/mark-attendance/mark-attendance.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import {NavController, AlertController, NavParams} from 'ionic-angular';
-import { ScanQRCodePage } from '../scan-qrcode/scan-qrcode';
-import { BarcodeScanner} from "@ionic-native/barcode-scanner";
+import {Component} from '@angular/core';
+import {AlertController, NavController, NavParams} from 'ionic-angular';
+import {ScanQRCodePage} from '../scan-qrcode/scan-qrcode';
+import {BarcodeScanner} from "@ionic-native/barcode-scanner";
 import {ViewCoursesPage} from "../view-courses/view-courses";
+import {AuthProvider} from "../../providers/auth/auth";
 
 
 @Component({
@@ -10,67 +11,108 @@ import {ViewCoursesPage} from "../view-courses/view-courses";
   templateUrl: 'mark-attendance.html',
 })
 export class MarkAttendancePage {
-  scannedCode=null;
-  public message:any;
-  public subMessage:any;
-  public studentObj:any;
-  public matchedSearch:any;
-  public matchedId:any;
+  scannedCode = null;
+  public message: any;
+  public subMessage: any;
+  public studentObj: any;
+  public matchedSearch: any;
+  public matchedId: any;
+  available: any;
+  public avlblStudents:any;
 
-  public selectWeek:any;
-  unit_Ob:any;
-  constructor(public navCtrl: NavController,public navParam:NavParams, private barcodeScanner : BarcodeScanner, public alertCtrl: AlertController) {
+  public selectWeek: any;
+  unit_Ob: any;
 
-    this.unit_Ob=this.navParam.data;
+  constructor(public navCtrl: NavController, public navParam: NavParams, private barcodeScanner: BarcodeScanner, public alertCtrl: AlertController,public authProvider:AuthProvider) {
+
+    this.unit_Ob = this.navParam.data;
     console.log(this.unit_Ob);
 
   }
 
-  scanStudentQRCode(){
+  checkAvailability(id, week) {
+
+    this.authProvider.checkWeekAvailability(id,week)
+      .then(data => {
+        this.available = data;
+        console.log(this.available);
+
+      });
+
+    return this.available;
+  }
+
+  getAvailableStudents(){
+
+    this.authProvider.getUnitStudents(this.unit_Ob.unit_id).then(data => {
+      this.avlblStudents = data;
+      console.log(this.avlblStudents);
+
+    });
+
+    this.avlblStudents=JSON.stringify(this.avlblStudents);
+    console.log(this.avlblStudents);
+    return this.avlblStudents;
+
+  }
+
+  scanStudentQRCode() {
 
     console.log(this.selectWeek);
 
-    this.barcodeScanner.scan().then(barcodeData => {
-      this.scannedCode = barcodeData.text;
-      var str = this.scannedCode;
-      str = str.substring(0, str.length-3);
-      this.scannedCode = str.toString().split('').pop();
-      //var studentId = 's3115458';
-      var studentIdArray = {
-        "students": [
-          { "name":"Student1", "student_id":"S310954", "user_id":"1" },
-          { "name":"Lecturer1", "student_id":"L234789", "user_id":"2" },
-          { "name":"Rock", "student_id":"S377488", "user_id":"3" },
-          { "name":"Rock", "student_id":"S987124", "user_id":"4" },
-          { "name":"Rock1", "student_id":"S123654", "user_id":"5" },
-          { "name":"RockFang", "student_id":"S897567", "user_id":"6" }
-        ]
-      }
+    if (this.checkAvailability(this.unit_Ob.unit_id, this.selectWeek)) {
 
-      this.studentObj = studentIdArray.students;
 
-      for (var i = 0; i < this.studentObj.length; i++) {
-        this.matchedSearch = this.studentObj[i].user_id;
-        if (this.matchedSearch == this.scannedCode){
-          this.matchedId = this.matchedSearch;
+      this.barcodeScanner.scan().then(barcodeData => {
+        this.scannedCode = barcodeData.text;
+        var str = this.scannedCode;
+        str = str.substring(0, str.length - 3);
+        this.scannedCode = str.toString().split('').pop();
+        //var studentId = 's3115458';
+
+
+
+        var studentIdArray=this.getAvailableStudents();
+
+        /*var studentIdArray = {
+          "students": [
+            {"name": "Student1", "student_id": "S310954", "user_id": "1"},
+            {"name": "Lecturer1", "student_id": "L234789", "user_id": "2"},
+            {"name": "Rock", "student_id": "S377488", "user_id": "3"},
+            {"name": "Rock", "student_id": "S987124", "user_id": "4"},
+            {"name": "Rock1", "student_id": "S123654", "user_id": "5"},
+            {"name": "RockFang", "student_id": "S897567", "user_id": "6"}
+          ]
+        }*/
+
+        this.studentObj = studentIdArray.students;
+
+        for (var i = 0; i < this.studentObj.length; i++) {
+          this.matchedSearch = this.studentObj[i].user_id;
+          if (this.matchedSearch == this.scannedCode) {
+            this.matchedId = this.matchedSearch;
+          }
         }
-      }
 
-      if (this.scannedCode == this.matchedId) {
-        this.showStudentScannedPopup('match');
-      }
-      else {
-        this.showStudentScannedPopup('unmatch');
-      }
-    }).catch(err => {
-      console.log('Error', err);
-    });
+        if (this.scannedCode == this.matchedId) {
+          this.showStudentScannedPopup('match');
+        }
+        else {
+          this.showStudentScannedPopup('unmatch');
+        }
+      }).catch(err => {
+        console.log('Error', err);
+      });
+
+    }else {
+      this.message=this.selectWeek + " already marked";
+    }
   }
-  goToScanQRCode(params){
+
+  goToScanQRCode(params) {
     if (!params) params = {};
     this.navCtrl.push(ScanQRCodePage);
   }
-
 
 
   showStudentScannedPopup(result) {
@@ -78,7 +120,7 @@ export class MarkAttendancePage {
       this.message = "Student marked successfully";
       this.subMessage = "Please press new student button to mark the next student or press finish button to end marking";
     }
-    else  {
+    else {
       this.message = "No student records found";
       this.subMessage = "Please press new student button to try again or press finish button to end marking";
     }
--- client/src/providers/auth/auth.ts
+++ client/src/providers/auth/auth.ts
@@ -1,5 +1,5 @@
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 /*
   Generated class for the AuthProvider provider.
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthProvider {
 
-  apiURL='https://cdu-mas.herokuapp.com/api';
+  apiURL = 'https://cdu-mas.herokuapp.com/api';
   // apiAdminURL='https://cdu-mas.herokuapp.com/api';
 
   public authToken;
@@ -22,8 +22,8 @@ export class AuthProvider {
 
 
   userLogin(data) {
-    return new Promise((resolve,reject)=>{
-      this.http.post(this.apiURL+'/auth/login', JSON.stringify(data), {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiURL + '/auth/login', JSON.stringify(data), {
         headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json'),
       })
         .subscribe(res => {
@@ -39,12 +39,12 @@ export class AuthProvider {
   }
 
 
-  userLogOut(){
+  userLogOut() {
 
-    return new Promise((resolve,reject)=>{
-      this.http.post(this.apiURL+'/auth/logout', "", {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiURL + '/auth/logout', "", {
         headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json')
-          .set('Authorization','Bearer'+this.getToken()),
+          .set('Authorization', 'Bearer' + this.getToken()),
       })
         .subscribe(res => {
           resolve(res);
@@ -58,9 +58,9 @@ export class AuthProvider {
   }
 
 
-  userRegistration(data){
-    return new Promise((resolve,reject)=>{
-      this.http.post(this.apiURL+'/register', JSON.stringify(data), {
+  userRegistration(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiURL + '/register', JSON.stringify(data), {
         headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json'),
       })
         .subscribe(res => {
@@ -75,12 +75,12 @@ export class AuthProvider {
 
   }
 
-  getUser(){
+  getUser() {
 
-    return new Promise((resolve,reject)=>{
-      this.http.post(this.apiURL+'/auth/me', "", {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.apiURL + '/auth/me', "", {
         headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json')
-          .set('Authorization','Bearer'+this.getToken()),
+          .set('Authorization', 'Bearer' + this.getToken()),
       })
         .subscribe(res => {
           resolve(res);
@@ -94,12 +94,12 @@ export class AuthProvider {
     });
   }
 
-  getUnits(){
-    return new Promise((resolve,reject)=>{
-      this.http.get(this.apiURL+'/unit', {
-        params: { id: this.userId},
+  getUnits() {
+    return new Promise((resolve, reject) => {
+      this.http.get(this.apiURL + '/unit', {
+        params: {id: this.userId},
         headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json')
-          .set('Authorization','Bearer'+this.getToken()),
+          .set('Authorization', 'Bearer' + this.getToken()),
       })
         .subscribe(res => {
           resolve(res);
@@ -113,15 +113,53 @@ export class AuthProvider {
   }
 
 
- storeToken(token) {
+  storeToken(token) {
 
-    window.localStorage.setItem("token",token);
+    window.localStorage.setItem("token", token);
     console.log("token saved");
 
   }
 
-getToken(){
+  getToken() {
     return window.localStorage.getItem("token");
-}
+  }
 
+
+  getUnitStudents(unit_id) {
+    return new Promise((resolve, reject) => {
+      this.http.get(this.apiURL + '/mark', {
+        params: {id: unit_id},
+        headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json')
+          .set('Authorization', 'Bearer' + this.getToken()),
+      })
+        .subscribe(res => {
+          resolve(res);
+          console.log(res);
+
+        }, (err) => {
+          reject(err);
+          console.log(err);
+        })
+    });
+
+  }
+
+  checkWeekAvailability(unit_id,week)
+  {
+    return new Promise((resolve, reject) => {
+      this.http.get(this.apiURL + '/mark/check', {
+        params: {id: unit_id,week: week},
+        headers: new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json')
+          .set('Authorization', 'Bearer' + this.getToken()),
+      })
+        .subscribe(res => {
+          resolve(res);
+          console.log(res);
+
+        }, (err) => {
+          reject(err);
+          console.log(err);
+        })
+    });
+  }
 }
